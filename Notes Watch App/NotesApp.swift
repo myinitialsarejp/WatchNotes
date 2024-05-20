78d03cb66fb262d7d5693eb9e182c3da40c34fcc
Jesse Pelletier <jespel2013@gmail.com>
2024-05-20 15:45:46 -0400

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Jesse Pelletier on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
