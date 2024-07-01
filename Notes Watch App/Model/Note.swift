af357e80d83a5a71dba8121e33a7ba265b03998b
Jesse Pelletier <jespel2013@gmail.com>
2024-07-01 12:32:46 -0400

    Notes app commit

---
+++ Notes Watch App/Model/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  Notes Watch App
+//
+//  Created by Jesse Pelletier on 6/2/24.
+//
+
+import Foundation
---
+++ Notes Watch App/View/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  Notes Watch App
+//
+//  Created by Jesse Pelletier on 7/1/24.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
